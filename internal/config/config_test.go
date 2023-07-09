b9671341d3c389a7d8166af124b670ebeb0caf3c
Alexandre Maldeme <alm@planday.com>
2023-07-09 18:38:33 +0200

    Add a middleware to control the max size of a clien request

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -15,9 +15,12 @@ func TestAppSetDefaults(t *testing.T) {
 	assert.Equal(t, defaultServerReadTimeout, app.ServerReadTimeout)
 	assert.Equal(t, defaultServerReadHeaderTimeout, app.ServerReadHeaderTimeout)
 	assert.Equal(t, defaultServerWriteTimeout, app.ServerWriteTimeout)
+	assert.Equal(t, defaultServerMaxRequestSize, app.ServerMaxRequestSize)
+
 	assert.Equal(t, defaultLoggerLogLevel, app.LoggerLogLevel)
 	assert.Equal(t, defaultLoggerDurationFieldUnit, app.LoggerDurationFieldUnit)
 	assert.Equal(t, defaultLoggerFormat, app.LoggerFormat)
+
 	assert.Equal(t, defaultClamavAddr, app.ClamavAddr)
 	assert.Equal(t, defaultClamavNetwork, app.ClamavNetwork)
 	assert.Equal(t, defaultClamavTimeout, app.ClamavTimeout)
